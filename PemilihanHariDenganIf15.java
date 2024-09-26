9e227e90a07b863f2c0b61a1f1618188660734e8
Aldyth11 <aldyth.r.f@gmail.com>
2024-09-26 08:59:37 +0700

    percobaan 2 no3

---
+++ PemilihanHariDenganIf15.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PemilihanHariDenganIf15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Masukkan angka (1-7): ");
+        int angka = sc.nextInt();
+
+        if (angka >= 1 && angka <= 5) {
+            System.out.println("Weekday");
+        } else if (angka == 6 || angka == 7) {
+            System.out.println("Weekend");
+        } else {
+            System.out.println("Invalid Number");
+        }
+    }
+}
