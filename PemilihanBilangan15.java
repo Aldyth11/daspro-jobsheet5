14f4de9df68828c482f29a0ee2ee273f87b5f4eb
Aldyth11 <aldyth.r.f@gmail.com>
2024-09-26 08:55:09 +0700

    percobaan1 no 1

--- PemilihanBilangan15.java
+++ PemilihanBilangan15.java
@@ -7,14 +7,15 @@ public class PemilihanBilangan15 {
        System.out.println("Masukkan sebuah angka: ");
        int angka = sc.nextInt();
 
-       if (angka % 2 == 0) 
-       {
-        System.out.println("Angka " + angka + " termasuk bilangan genap");
-       }
-       else 
-        System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+    //    if (angka % 2 == 0) 
+    //    {
+    //     System.out.println("Angka " + angka + " termasuk bilangan genap");
+    //    }
+    //    else 
+    //     System.out.println("Angka " + angka + " termasuk bilangan ganjil");
 
-        
+        String hasil = angka % 2 == 0 ? "bilangan genap" : "bilangan ganjil";
+        System.out.println(hasil);
     }
     
 }
